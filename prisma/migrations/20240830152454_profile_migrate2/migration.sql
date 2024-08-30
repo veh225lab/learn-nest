9edbadf7f0d11310197d349803698c8f3708b3ca
veh225lab <vehnickgoueu@gmail.com>
2024-08-30 16:08:01 +0000

    Initial commit

---
+++ prisma/migrations/20240830152454_profile_migrate2/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - The primary key for the `Profile` table will be changed. If it partially fails, the table could be left without primary key constraint.
+
+*/
+-- AlterTable
+ALTER TABLE "Profile" DROP CONSTRAINT "Profile_pkey",
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ADD CONSTRAINT "Profile_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "Profile_id_seq";
